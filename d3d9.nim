6d9c3f75cf54d808c4c59687fa45e66b861c785f
SchelZ <67759165+SchelZ@users.noreply.github.com>
2023-12-06 03:05:30 +0200

    Update d3d9.nim

--- d3d9.nim
+++ d3d9.nim
@@ -7,8 +7,45 @@ else:
 {.pragma: d3d9_header, header: "Include/d3d9.h".}
 {.pragma: d3d9types_header, header: "Include/d3d9types.h".}
 
-const 
 
+const 
+  D3D_SDK_VERSION*: uint = 31
+  D3DADAPTER_DEFAULT*: uint = 0
+  D3DCREATE_SOFTWARE_VERTEXPROCESSING*: DWORD = 0x00000020
+  D3DCREATE_HARDWARE_VERTEXPROCESSING*: DWORD = 0x00000040
+  D3DCLEAR_TARGET*: DWORD = 0x00000001
+
+  D3DFVF_RESERVED0* = 0x001
+  D3DFVF_POSITION_MASK* = 0x400E
+  D3DFVF_XYZ* = 0x002
+  D3DFVF_XYZRHW* = 0x004
+  D3DFVF_XYZB1* = 0x006
+  D3DFVF_XYZB2* = 0x008
+  D3DFVF_XYZB3* = 0x00a
+  D3DFVF_XYZB4* = 0x00c
+  D3DFVF_XYZB5* = 0x00e
+  D3DFVF_XYZW* = 0x4002
+
+  D3DFVF_NORMAL* = 0x010
+  D3DFVF_PSIZE* = 0x020
+  D3DFVF_DIFFUSE* = 0x040
+  D3DFVF_SPECULAR* = 0x080
+
+  D3DFVF_TEXCOUNT_MASK* = 0xf00
+  D3DFVF_TEXCOUNT_SHIFT* = 8
+  D3DFVF_TEX0* = 0x000
+  D3DFVF_TEX1* = 0x100
+  D3DFVF_TEX2* = 0x200
+  D3DFVF_TEX3* = 0x300
+  D3DFVF_TEX4* = 0x400
+  D3DFVF_TEX5* = 0x500
+  D3DFVF_TEX6* = 0x600
+  D3DFVF_TEX7* = 0x700
+  D3DFVF_TEX8* = 0x800
+
+  D3DFVF_LASTBETA_UBYTE4* = 0x1000
+  D3DFVF_LASTBETA_D3DCOLOR* = 0x8000
+  D3DFVF_RESERVED2* = 0x6000  # 2 reserved bits
 
   # destination parameter 
   D3DSP_DSTSHIFT_SHIFT* = 24
@@ -103,6 +140,26 @@ template D3DFVF_TEXCOORDSIZE1*(CoordIndex: untyped): untyped = (D3DFVF_TEXTUREFO
 
 
 type 
+
+  D3DSHADEMODE* {.importcpp: "enum _D3DSHADEMODE", d3d9types_header, pure.} = enum
+    D3DSHADE_FLAT               = 1,
+    D3DSHADE_GOURAUD            = 2,
+    D3DSHADE_PHONG              = 3,
+    D3DSHADE_FORCE_DWORD        = 0x7fffffff # force 32-bit size enum */
+
+  D3DFILLMODE* {.importcpp: "enum _D3DFILLMODE", d3d9types_header, pure, size: int32.sizeof.} = enum
+    D3DFILL_POINT               = 1,
+    D3DFILL_WIREFRAME           = 2,
+    D3DFILL_SOLID               = 3,
+    D3DFILL_FORCE_DWORD         = 0x7fffffff # force 32-bit size enum */
+
+  D3DVIEWPORT9* {.importcpp: "enum _D3DVIEWPORT9", d3d9types_header, pure.} = object
+    X*: DWORD
+    Y*: DWORD             # Viewport Top left */
+    Width*: DWORD
+    Height*: DWORD       # Viewport Dimensions */
+    MinZ*: float         # Min/max of clip Volume */
+    MaxZ*: float
 
   # destination/source parameter register type
   #
@@ -389,6 +446,113 @@ type
     D3DRTYPE_INDEXBUFFER            =  7,
     D3DRTYPE_FORCE_DWORD            = 0x7fffffff
 
+  D3DRENDERSTATETYPE* {.importcpp: "enum _D3DRENDERSTATETYPE", d3d9types_header, pure, size: int32.sizeof.} = enum
+    D3DRS_ZENABLE                   = 7,    # D3DZBUFFERTYPE (or TRUE/FALSE for legacy) */
+    D3DRS_FILLMODE                  = 8,    # D3DFILLMODE */
+    D3DRS_SHADEMODE                 = 9,    # D3DSHADEMODE */
+    D3DRS_ZWRITEENABLE              = 14,   # TRUE to enable z writes */
+    D3DRS_ALPHATESTENABLE           = 15,   # TRUE to enable alpha tests */
+    D3DRS_LASTPIXEL                 = 16,   # TRUE for last-pixel on lines */
+    D3DRS_SRCBLEND                  = 19,   # D3DBLEND */
+    D3DRS_DESTBLEND                 = 20,   # D3DBLEND */
+    D3DRS_CULLMODE                  = 22,   # D3DCULL */
+    D3DRS_ZFUNC                     = 23,   # D3DCMPFUNC */
+    D3DRS_ALPHAREF                  = 24,   # D3DFIXED */
+    D3DRS_ALPHAFUNC                 = 25,   # D3DCMPFUNC */
+    D3DRS_DITHERENABLE              = 26,   # TRUE to enable dithering */
+    D3DRS_ALPHABLENDENABLE          = 27,   # TRUE to enable alpha blending */
+    D3DRS_FOGENABLE                 = 28,   # TRUE to enable fog blending */
+    D3DRS_SPECULARENABLE            = 29,   # TRUE to enable specular */
+    D3DRS_FOGCOLOR                  = 34,   # D3DCOLOR */
+    D3DRS_FOGTABLEMODE              = 35,   # D3DFOGMODE */
+    D3DRS_FOGSTART                  = 36,   # Fog start (for both vertex and pixel fog) */
+    D3DRS_FOGEND                    = 37,   # Fog end      */
+    D3DRS_FOGDENSITY                = 38,   # Fog density  */
+    D3DRS_RANGEFOGENABLE            = 48,   # Enables range-based fog */
+    D3DRS_STENCILENABLE             = 52,   # BOOL enable/disable stenciling */
+    D3DRS_STENCILFAIL               = 53,   # D3DSTENCILOP to do if stencil test fails */
+    D3DRS_STENCILZFAIL              = 54,   # D3DSTENCILOP to do if stencil test passes and Z test fails */
+    D3DRS_STENCILPASS               = 55,   # D3DSTENCILOP to do if both stencil and Z tests pass */
+    D3DRS_STENCILFUNC               = 56,   # D3DCMPFUNC fn.  Stencil Test passes if ((ref & mask) stencilfn (stencil & mask)) is true */
+    D3DRS_STENCILREF                = 57,   # Reference value used in stencil test */
+    D3DRS_STENCILMASK               = 58,   # Mask value used in stencil test */
+    D3DRS_STENCILWRITEMASK          = 59,   # Write mask applied to values written to stencil buffer */
+    D3DRS_TEXTUREFACTOR             = 60,   # D3DCOLOR used for multi-texture blend */
+    D3DRS_WRAP0                     = 128,  # wrap for 1st texture coord. set */
+    D3DRS_WRAP1                     = 129,  # wrap for 2nd texture coord. set */
+    D3DRS_WRAP2                     = 130,  # wrap for 3rd texture coord. set */
+    D3DRS_WRAP3                     = 131,  # wrap for 4th texture coord. set */
+    D3DRS_WRAP4                     = 132,  # wrap for 5th texture coord. set */
+    D3DRS_WRAP5                     = 133,  # wrap for 6th texture coord. set */
+    D3DRS_WRAP6                     = 134,  # wrap for 7th texture coord. set */
+    D3DRS_WRAP7                     = 135,  # wrap for 8th texture coord. set */
+    D3DRS_CLIPPING                  = 136,
+    D3DRS_LIGHTING                  = 137,
+    D3DRS_AMBIENT                   = 139,
+    D3DRS_FOGVERTEXMODE             = 140,
+    D3DRS_COLORVERTEX               = 141,
+    D3DRS_LOCALVIEWER               = 142,
+    D3DRS_NORMALIZENORMALS          = 143,
+    D3DRS_DIFFUSEMATERIALSOURCE     = 145,
+    D3DRS_SPECULARMATERIALSOURCE    = 146,
+    D3DRS_AMBIENTMATERIALSOURCE     = 147,
+    D3DRS_EMISSIVEMATERIALSOURCE    = 148,
+    D3DRS_VERTEXBLEND               = 151,
+    D3DRS_CLIPPLANEENABLE           = 152,
+    D3DRS_POINTSIZE                 = 154,   # float point size */
+    D3DRS_POINTSIZE_MIN             = 155,   # float point size min threshold */
+    D3DRS_POINTSPRITEENABLE         = 156,   # BOOL point texture coord control */
+    D3DRS_POINTSCALEENABLE          = 157,   # BOOL point size scale enable */
+    D3DRS_POINTSCALE_A              = 158,   # float point attenuation A value */
+    D3DRS_POINTSCALE_B              = 159,   # float point attenuation B value */
+    D3DRS_POINTSCALE_C              = 160,   # float point attenuation C value */
+    D3DRS_MULTISAMPLEANTIALIAS      = 161,  #BOOL - set to do FSAA with multisample buffer
+    D3DRS_MULTISAMPLEMASK           = 162,  #DWORD - per-sample enable/disable
+    D3DRS_PATCHEDGESTYLE            = 163,  #Sets whether patch edges will use float style tessellation
+    D3DRS_DEBUGMONITORTOKEN         = 165,  #DEBUG ONLY - token to debug monitor
+    D3DRS_POINTSIZE_MAX             = 166,   # float point size max threshold */
+    D3DRS_INDEXEDVERTEXBLENDENABLE  = 167,
+    D3DRS_COLORWRITEENABLE          = 168,  #per-channel write enable
+    D3DRS_TWEENFACTOR               = 170,   #float tween factor
+    D3DRS_BLENDOP                   = 171,   #D3DBLENDOP setting
+    D3DRS_POSITIONDEGREE            = 172,   #NPatch position interpolation degree. D3DDEGREE_LINEAR or D3DDEGREE_CUBIC (default)
+    D3DRS_NORMALDEGREE              = 173,   #NPatch normal interpolation degree. D3DDEGREE_LINEAR (default) or D3DDEGREE_QUADRATIC
+    D3DRS_SCISSORTESTENABLE         = 174,
+    D3DRS_SLOPESCALEDEPTHBIAS       = 175,
+    D3DRS_ANTIALIASEDLINEENABLE     = 176,
+    D3DRS_MINTESSELLATIONLEVEL      = 178,
+    D3DRS_MAXTESSELLATIONLEVEL      = 179,
+    D3DRS_ADAPTIVETESS_X            = 180,
+    D3DRS_ADAPTIVETESS_Y            = 181,
+    D3DRS_ADAPTIVETESS_Z            = 182,
+    D3DRS_ADAPTIVETESS_W            = 183,
+    D3DRS_ENABLEADAPTIVETESSELLATION = 184,
+    D3DRS_TWOSIDEDSTENCILMODE       = 185,   # BOOL enable/disable 2 sided stenciling */
+    D3DRS_CCW_STENCILFAIL           = 186,   # D3DSTENCILOP to do if ccw stencil test fails */
+    D3DRS_CCW_STENCILZFAIL          = 187,   # D3DSTENCILOP to do if ccw stencil test passes and Z test fails */
+    D3DRS_CCW_STENCILPASS           = 188,   # D3DSTENCILOP to do if both ccw stencil and Z tests pass */
+    D3DRS_CCW_STENCILFUNC           = 189,   # D3DCMPFUNC fn.  ccw Stencil Test passes if ((ref & mask) stencilfn (stencil & mask)) is true */
+    D3DRS_COLORWRITEENABLE1         = 190,   # Additional ColorWriteEnables for the devices that support D3DPMISCCAPS_INDEPENDENTWRITEMASKS */
+    D3DRS_COLORWRITEENABLE2         = 191,   # Additional ColorWriteEnables for the devices that support D3DPMISCCAPS_INDEPENDENTWRITEMASKS */
+    D3DRS_COLORWRITEENABLE3         = 192,   # Additional ColorWriteEnables for the devices that support D3DPMISCCAPS_INDEPENDENTWRITEMASKS */
+    D3DRS_BLENDFACTOR               = 193,   # D3DCOLOR used for a constant blend factor during alpha blending for devices that support D3DPBLENDCAPS_BLENDFACTOR */
+    D3DRS_SRGBWRITEENABLE           = 194,   # Enable rendertarget writes to be DE-linearized to SRGB (for formats that expose D3DUSAGE_QUERY_SRGBWRITE) */
+    D3DRS_DEPTHBIAS                 = 195,
+    D3DRS_WRAP8                     = 198,   # Additional wrap states for vs_3_0+ attributes with D3DDECLUSAGE_TEXCOORD */
+    D3DRS_WRAP9                     = 199,
+    D3DRS_WRAP10                    = 200,
+    D3DRS_WRAP11                    = 201,
+    D3DRS_WRAP12                    = 202,
+    D3DRS_WRAP13                    = 203,
+    D3DRS_WRAP14                    = 204,
+    D3DRS_WRAP15                    = 205,
+    D3DRS_SEPARATEALPHABLENDENABLE  = 206,  # TRUE to enable a separate blending function for the alpha channel */
+    D3DRS_SRCBLENDALPHA             = 207,  # SRC blend factor for the alpha channel when D3DRS_SEPARATEDESTALPHAENABLE is TRUE */
+    D3DRS_DESTBLENDALPHA            = 208,  # DST blend factor for the alpha channel when D3DRS_SEPARATEDESTALPHAENABLE is TRUE */
+    D3DRS_BLENDOPALPHA              = 209,  # Blending operation for the alpha channel when D3DRS_SEPARATEDESTALPHAENABLE is TRUE */
+    D3DRS_FORCE_DWORD               = 0x7fffffff, # force 32-bit size enum */
+
+
   D3DRECT* {.importcpp: "struct _D3DRECT", d3d9types_header, pure.} = object
     x1*: LONG
     y1*: LONG
@@ -412,8 +576,27 @@ type
     rdh*: RGNDATAHEADER
     Buffer*: array[1, char]
 
+# typedef interface IDirect3DResource9            IDirect3DResource9;
+# typedef interface IDirect3DBaseTexture9         IDirect3DBaseTexture9;
+# typedef interface IDirect3DTexture9             IDirect3DTexture9;
+# typedef interface IDirect3DVolumeTexture9       IDirect3DVolumeTexture9;
+# typedef interface IDirect3DCubeTexture9         IDirect3DCubeTexture9;
+# typedef interface IDirect3DVertexBuffer9        IDirect3DVertexBuffer9;
+# typedef interface IDirect3DIndexBuffer9         IDirect3DIndexBuffer9;
+# typedef interface IDirect3DSurface9             IDirect3DSurface9;
+# typedef interface IDirect3DVolume9              IDirect3DVolume9;
+# typedef interface IDirect3DSwapChain9           IDirect3DSwapChain9;
+# typedef interface IDirect3DQuery9               IDirect3DQuery9;
 
-type
+type 
+  #---------------------------------------------------------------------
+  IDirect3D9* {.importcpp: "IDirect3D9",  d3d9_header, inheritable, pure.} = object
+    Release*: proc (): ULONG {.stdcall.}
+    CreateDevice*: proc (Adapter: uint, DeviceType: D3DDEVTYPE, hFocusWindow: HWND, BehaviorFlags: DWORD, pPresentationParameters: ptr D3DPRESENT_PARAMETERS, ppReturnedDeviceInterface: ptr ptr IDirect3DDevice9): HRESULT {.stdcall.}
+  LPDIRECT3D9* {.importcpp: "LPDIRECT3D9",  d3d9_header.} = ptr IDirect3D9
+  PDIRECT3D9* {.importcpp: "PDIRECT3D9",  d3d9_header.} = ptr IDirect3D9
+
+  #---------------------------------------------------------------------
   IDirect3DDevice9* {.importcpp: "IDirect3DDevice9", d3d9_header, inheritable, pure.} = object
     Release*: proc (): ULONG  {.stdcall.}
     Clear*: proc (Count: DWORD, pRects: ptr D3DRECT, Flags: DWORD, Color: DWORD, Z: float, Stencil: DWORD): HRESULT {.stdcall.}
@@ -421,24 +604,73 @@ type
     EndScene*: proc (): HRESULT {.stdcall.}
     Present*: proc (pSourceRect: ptr RECTT, pDestRect: ptr RECTT, hDestWindowOverride: HWND, pDirtyRegion: ptr RGNDATA): HRESULT {.stdcall.}
     Reset*: proc (pPresentationParameters: ptr D3DPRESENT_PARAMETERS): HRESULT {.stdcall.}
-  
+    SetVertexShader*: proc (pShader: ptr IDirect3DVertexShader9): HRESULT {.stdcall.}
+    GetVertexShader*: proc (pShader: ptr ptr IDirect3DVertexShader9): HRESULT {.stdcall.}
+    SetViewport*: proc (pViewport: ptr D3DVIEWPORT9): HRESULT {.stdcall.}
+    SetPixelShader*: proc (pShader: ptr IDirect3DPixelShader9): HRESULT {.stdcall.}
+    GetPixelShader*: proc (ppShader: ptr ptr IDirect3DPixelShader9): HRESULT {.stdcall.}
+    SetRenderState*: proc (State: D3DRENDERSTATETYPE, Value: DWORD): HRESULT {.stdcall.}
+    GetRenderState*: proc (State: D3DRENDERSTATETYPE, pValue: ptr DWORD): HRESULT {.stdcall.}
+
   LPDIRECT3DDEVICE9* {.importcpp: "LPDIRECT3DDEVICE9", d3d9_header.} = ptr IDirect3DDevice9
   PDIRECT3DDEVICE9* {.importcpp: "PDIRECT3DDEVICE9", d3d9_header.} = ptr IDirect3DDevice9
 
-type
-  IDirect3D9* {.importcpp: "IDirect3D9",  d3d9_header, inheritable, pure.} = object
-    Release*: proc (): ULONG {.stdcall.}
-    CreateDevice*: proc (Adapter: uint, DeviceType: D3DDEVTYPE, hFocusWindow: HWND, BehaviorFlags: DWORD, pPresentationParameters: ptr D3DPRESENT_PARAMETERS, ppReturnedDeviceInterface: ptr ptr IDirect3DDevice9): HRESULT {.stdcall.}
+  #---------------------------------------------------------------------
+  IDirect3DStateBlock9* {.importcpp: "IDirect3DStateBlock9",  d3d9_header, inheritable, pure.} = object
+    AddRef*: proc(): ULONG {.stdcall.}
+    Release*: proc(): ULONG {.stdcall.}
 
-  LPDIRECT3D9* {.importcpp: "LPDIRECT3D9",  d3d9_header.} = ptr IDirect3D9
-  PDIRECT3D9* {.importcpp: "PDIRECT3D9",  d3d9_header.} = ptr IDirect3D9
+    #*** IDirect3DStateBlock9 methods ***#
+    GetDevice*: proc(ppDevice: ptr ptr IDirect3DDevice9): HRESULT {.stdcall.}
+    Capture*: proc(): HRESULT {.stdcall.}
+    Apply*: proc(): HRESULT {.stdcall.}
+  LPDIRECT3DSTATEBLOCK9* {.importcpp: "LPDIRECT3DSTATEBLOCK9",  d3d9_header.} = ptr IDirect3DStateBlock9
+  PDIRECT3DSTATEBLOCK9* {.importcpp: "PDIRECT3DSTATEBLOCK9",  d3d9_header.} = ptr IDirect3DStateBlock9
 
-const 
-  D3D_SDK_VERSION*: uint = 31
-  D3DADAPTER_DEFAULT*: uint = 0
-  D3DCREATE_SOFTWARE_VERTEXPROCESSING*: DWORD = 0x00000020
-  D3DCREATE_HARDWARE_VERTEXPROCESSING*: DWORD = 0x00000040
-  D3DCLEAR_TARGET*: DWORD = 0x00000001
+  #---------------------------------------------------------------------
+  IDirect3DVertexDeclaration9* {.importcpp: "IDirect3DVertexDeclaration9",  d3d9_header, inheritable, pure.} = object
+    AddRef*: proc(): ULONG {.stdcall.}
+    Release*: proc(): ULONG {.stdcall.}
+
+    #*** IDirect3DVertexDeclaration9 methods ***#
+    GetDevice*: proc(ppDevice: ptr ptr IDirect3DDevice9): HRESULT {.stdcall.}
+  LPDIRECT3DVERTEXDECLARATION9* {.importcpp: "LPDIRECT3DVERTEXDECLARATION9",  d3d9_header.} = ptr IDirect3DVertexDeclaration9
+  PDIRECT3DVERTEXDECLARATION9* {.importcpp: "PDIRECT3DVERTEXDECLARATION9",  d3d9_header.} = ptr IDirect3DVertexDeclaration9
+
+  #---------------------------------------------------------------------
+  IDirect3DVertexShader9* {.importcpp: "IDirect3DVertexShader9",  d3d9_header, inheritable, pure.} = object
+    AddRef*: proc(): ULONG {.stdcall.}
+    Release*: proc(): ULONG {.stdcall.}
+
+    #*** IDirect3DVertexShader9 methods ***#
+    GetDevice*: proc(ppDevice: ptr ptr IDirect3DDevice9): HRESULT {.stdcall.}
+  LPDIRECT3DVERTEXSHADER9* {.importcpp: "LPDIRECT3DVERTEXSHADER9",  d3d9_header.} = ptr IDirect3DVertexShader9
+  PDIRECT3DVERTEXSHADER9* {.importcpp: "PDIRECT3DVERTEXSHADER9",  d3d9_header.} = ptr IDirect3DVertexShader9
+
+  #---------------------------------------------------------------------
+  IDirect3DPixelShader9* {.importcpp: "IDirect3DPixelShader9", d3d9_header, inheritable, pure.} = object
+    AddRef*: proc(): ULONG {.stdcall.}
+    Release*: proc(): ULONG {.stdcall.}
+
+    #*** IDirect3DPixelShader9 methods ***#
+    GetDevice*: proc(ppDevice: ptr ptr IDirect3DDevice9): HRESULT {.stdcall.}
+  LPDIRECT3DPIXELSHADER9* {.importcpp: "LPDIRECT3DPIXELSHADER9", d3d9_header.} = ptr IDirect3DPixelShader9
+  PDIRECT3DPIXELSHADER9* {.importcpp: "PDIRECT3DPIXELSHADER9", d3d9_header.} = ptr IDirect3DPixelShader9
+
+
+  IDirect3DVertexBuffer9* {.importcpp: "IDirect3DVertexBuffer9", d3d9_header, inheritable, pure.} = object
+  LPDIRECT3DVERTEXBUFFER9* {.importcpp: "LPDIRECT3DVERTEXBUFFER9", d3d9_header.} = ptr IDirect3DVertexBuffer9
+  PDIRECT3DVERTEXBUFFER9* {.importcpp: "PDIRECT3DVERTEXBUFFER9", d3d9_header.} = ptr IDirect3DVertexBuffer9
+
+
+  IDirect3DIndexBuffer9* {.importcpp: "IDirect3DIndexBuffer9", d3d9_header, inheritable, pure.} = object
+  LPDIRECT3DINDEXBUFFER9* {.importcpp: "LPDIRECT3DINDEXBUFFER9", d3d9_header.} = ptr IDirect3DIndexBuffer9
+  PDIRECT3DINDEXBUFFER9* {.importcpp: "PDIRECT3DINDEXBUFFER9", d3d9_header.} = ptr IDirect3DIndexBuffer9
+
+
+  IDirect3DTexture9* {.importcpp: "IDirect3DTexture9",  d3d9_header, inheritable, pure.} = object
+  LPDIRECT3DTEXTURE9* {.importcpp: "LPDIRECT3DTEXTURE9", d3d9_header.} = ptr IDirect3DTexture9
+  PDIRECT3DTEXTURE9* {.importcpp: "PDIRECT3DTEXTURE9", d3d9_header.} = ptr IDirect3DTexture9 
 
 func Direct3DCreate9*(SDKVersion: uint): ptr IDirect3D9 {.importcpp: "Direct3DCreate9(@)", d3d9_header, stdcall.}
 
