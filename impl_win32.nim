acd987bc1ac79f64e4b067b86ab2dcdaf1420280
Mihai <67759165+SchelZ@users.noreply.github.com>
2025-12-19 22:44:40 +0200

    Update impl_win32.nim

--- impl_win32.nim
+++ impl_win32.nim
@@ -1,20 +1,18 @@
 import winim/lean
 import nimgl/imgui
 
-# ------------------------------------------------------------
-# Backend state
-# ------------------------------------------------------------
-
 var
   g_hWnd: HWND = 0
   g_Time: int64 = 0
   g_TicksPerSecond: int64 = 0
+  g_MouseJustPressed: array[5, bool]  # 0..4
+  g_MouseCursors: array[32, HCURSOR]
+  g_LastMouseCursor: int32 = -1
 
-# ------------------------------------------------------------
-# Init / Shutdown
-# ------------------------------------------------------------
+const
+  DBT_DEVNODES_CHANGED* = 0x0007
 
-proc igWin32Init*(hwnd: HWND): bool {.discardable.} =
+proc igWin32Init*(hwnd: HWND; installImeHandle: bool = true): bool {.discardable.} =
   g_hWnd = hwnd
 
   if QueryPerformanceFrequency(cast[ptr LARGE_INTEGER](addr g_TicksPerSecond)) == 0:
@@ -23,111 +21,62 @@ proc igWin32Init*(hwnd: HWND): bool {.discardable.} =
     return false
 
   let io = igGetIO()
+  io.backendPlatformName = "imgui_impl_win32_nim"
 
-  io.backendPlatformName = "imgui_impl_win32"
-  io.backendFlags = ImGuiBackendFlags(
+  # Backend flags (legacy style in NimGL)
+  io.backendFlags = cast[ImGuiBackendFlags](
     io.backendFlags.int32 or
     ImGuiBackendFlags.HasMouseCursors.int32 or
     ImGuiBackendFlags.HasSetMousePos.int32
   )
 
-  io.imeWindowHandle = cast[pointer](hwnd)
 
-  # Key mapping (exactly like C++)
+  if installImeHandle:
+    io.imeWindowHandle = cast[pointer](g_hWnd)
+
+  # Legacy key mapping like the NimGL GLFW backend example
   io.keyMap[ImGuiKey.Tab.int32]        = VK_TAB
-  io.keyMap[ImGuiKey.LeftArrow.int32] = VK_LEFT
-  io.keyMap[ImGuiKey.RightArrow.int32]= VK_RIGHT
-  io.keyMap[ImGuiKey.UpArrow.int32]   = VK_UP
-  io.keyMap[ImGuiKey.DownArrow.int32] = VK_DOWN
-  io.keyMap[ImGuiKey.PageUp.int32]    = VK_PRIOR
-  io.keyMap[ImGuiKey.PageDown.int32]  = VK_NEXT
-  io.keyMap[ImGuiKey.Home.int32]      = VK_HOME
-  io.keyMap[ImGuiKey.End.int32]       = VK_END
-  io.keyMap[ImGuiKey.Insert.int32]    = VK_INSERT
-  io.keyMap[ImGuiKey.Delete.int32]    = VK_DELETE
-  io.keyMap[ImGuiKey.Backspace.int32] = VK_BACK
-  io.keyMap[ImGuiKey.Space.int32]     = VK_SPACE
-  io.keyMap[ImGuiKey.Enter.int32]     = VK_RETURN
-  io.keyMap[ImGuiKey.Escape.int32]    = VK_ESCAPE
-  io.keyMap[ImGuiKey.KeyPadEnter.int32] = VK_RETURN
-  io.keyMap[ImGuiKey.A.int32] = 'A'.ord
-  io.keyMap[ImGuiKey.C.int32] = 'C'.ord
-  io.keyMap[ImGuiKey.V.int32] = 'V'.ord
-  io.keyMap[ImGuiKey.X.int32] = 'X'.ord
-  io.keyMap[ImGuiKey.Y.int32] = 'Y'.ord
-  io.keyMap[ImGuiKey.Z.int32] = 'Z'.ord
+  io.keyMap[ImGuiKey.LeftArrow.int32]  = VK_LEFT
+  io.keyMap[ImGuiKey.RightArrow.int32] = VK_RIGHT
+  io.keyMap[ImGuiKey.UpArrow.int32]    = VK_UP
+  io.keyMap[ImGuiKey.DownArrow.int32]  = VK_DOWN
+  io.keyMap[ImGuiKey.PageUp.int32]     = VK_PRIOR
+  io.keyMap[ImGuiKey.PageDown.int32]   = VK_NEXT
+  io.keyMap[ImGuiKey.Home.int32]       = VK_HOME
+  io.keyMap[ImGuiKey.End.int32]        = VK_END
+  io.keyMap[ImGuiKey.Insert.int32]     = VK_INSERT
+  io.keyMap[ImGuiKey.Delete.int32]     = VK_DELETE
+  io.keyMap[ImGuiKey.Backspace.int32]  = VK_BACK
+  io.keyMap[ImGuiKey.Space.int32]      = VK_SPACE
+  io.keyMap[ImGuiKey.Enter.int32]      = VK_RETURN
+  io.keyMap[ImGuiKey.Escape.int32]     = VK_ESCAPE
+  io.keyMap[ImGuiKey.A.int32]          = 0x41
+  io.keyMap[ImGuiKey.C.int32]          = 0x43
+  io.keyMap[ImGuiKey.V.int32]          = 0x56
+  io.keyMap[ImGuiKey.X.int32]          = 0x58
+  io.keyMap[ImGuiKey.Y.int32]          = 0x59
+  io.keyMap[ImGuiKey.Z.int32]          = 0x5A
 
-  true
+  # Cursor handles (map common ones; anything missing will just fallback to arrow)
+  g_MouseCursors[0] = LoadCursorW(0, IDC_ARROW)     # Arrow
+  g_MouseCursors[1] = LoadCursorW(0, IDC_IBEAM)     # TextInput
+  g_MouseCursors[2] = LoadCursorW(0, IDC_SIZEALL)   # ResizeAll
+  g_MouseCursors[3] = LoadCursorW(0, IDC_SIZEWE)    # ResizeEW
+  g_MouseCursors[4] = LoadCursorW(0, IDC_SIZENS)    # ResizeNS
+  g_MouseCursors[5] = LoadCursorW(0, IDC_SIZENESW)  # ResizeNESW
+  g_MouseCursors[6] = LoadCursorW(0, IDC_SIZENWSE)  # ResizeNWSE
+  g_MouseCursors[7] = LoadCursorW(0, IDC_HAND)      # Hand
+  g_MouseCursors[8] = LoadCursorW(0, IDC_NO)        # NotAllowed
+  g_MouseCursors[9] = LoadCursorW(0, IDC_WAIT)      # Wait (if your enum uses it)
+  g_MouseCursors[10] = LoadCursorW(0, IDC_APPSTARTING) # Progress (if your enum uses it)
 
+  return true
 
 proc igWin32Shutdown*() =
   g_hWnd = 0
+  g_LastMouseCursor = -1
 
-# ------------------------------------------------------------
-# Mouse cursor
-# ------------------------------------------------------------
-
-proc igWin32UpdateMouseCursor*(): bool =
-  let io = igGetIO()
-  if (io.configFlags.int32 and ImGuiConfigFlags.NoMouseCursorChange.int32) != 0:
-    return false
-
-  let imguiCursor = igGetMouseCursor()
-  if imguiCursor == ImGuiMouseCursor.None or io.mouseDrawCursor:
-    SetCursor(0)
-    return true
-
-  var winCursor: LPCWSTR = IDC_ARROW
-  case imguiCursor:
-    of ImGuiMouseCursor.Arrow:        winCursor = IDC_ARROW
-    of ImGuiMouseCursor.TextInput:    winCursor = IDC_IBEAM
-    of ImGuiMouseCursor.ResizeAll:    winCursor = IDC_SIZEALL
-    of ImGuiMouseCursor.ResizeEW:     winCursor = IDC_SIZEWE
-    of ImGuiMouseCursor.ResizeNS:     winCursor = IDC_SIZENS
-    of ImGuiMouseCursor.ResizeNESW:   winCursor = IDC_SIZENESW
-    of ImGuiMouseCursor.ResizeNWSE:   winCursor = IDC_SIZENWSE
-    of ImGuiMouseCursor.Hand:         winCursor = IDC_HAND
-    of ImGuiMouseCursor.NotAllowed:   winCursor = IDC_NO
-    else: discard
-
-  SetCursor(LoadCursorW(0, winCursor))
-  true
-
-# ------------------------------------------------------------
-# Per-frame update
-# ------------------------------------------------------------
-
-proc igWin32NewFrame*() =
-  let io = igGetIO()
-
-  var rect: RECT
-  GetClientRect(g_hWnd, rect.addr)
-  io.displaySize = ImVec2(
-    x: float32(rect.right - rect.left),
-    y: float32(rect.bottom - rect.top)
-  )
-
-  var currentTime: int64
-  QueryPerformanceCounter(cast[ptr LARGE_INTEGER](addr currentTime))
-  io.deltaTime = float32(currentTime - g_Time) / float32(g_TicksPerSecond)
-  g_Time = currentTime
-
-  io.keyCtrl  = (GetKeyState(VK_CONTROL) and 0x8000) != 0
-  io.keyShift = (GetKeyState(VK_SHIFT) and 0x8000) != 0
-  io.keyAlt   = (GetKeyState(VK_MENU) and 0x8000) != 0
-  io.keySuper = false
-
-# ------------------------------------------------------------
-# Win32 message handler
-# ------------------------------------------------------------
-
-proc igWin32WndProcHandler*(
-  hwnd: HWND,
-  msg: UINT,
-  wParam: WPARAM,
-  lParam: LPARAM
-): LRESULT {.discardable.} =
-
+proc igWin32WndProcHandler*(hwnd: HWND, msg: UINT, wParam: WPARAM, lParam: LPARAM): LRESULT {.stdcall.} =
   if igGetCurrentContext() == nil:
     return 0
 
@@ -135,62 +84,121 @@ proc igWin32WndProcHandler*(
 
   case msg
   of WM_LBUTTONDOWN, WM_LBUTTONDBLCLK:
-    io.mouseDown[0] = true
-    SetCapture(hwnd)
+    g_MouseJustPressed[0] = true
+    if GetCapture() == 0: SetCapture(hwnd)
     return 0
-
   of WM_LBUTTONUP:
     io.mouseDown[0] = false
-    ReleaseCapture()
+    if GetCapture() == hwnd: ReleaseCapture()
     return 0
 
   of WM_RBUTTONDOWN, WM_RBUTTONDBLCLK:
-    io.mouseDown[1] = true
-    SetCapture(hwnd)
+    g_MouseJustPressed[1] = true
+    if GetCapture() == 0: SetCapture(hwnd)
     return 0
-
   of WM_RBUTTONUP:
     io.mouseDown[1] = false
-    ReleaseCapture()
+    if GetCapture() == hwnd: ReleaseCapture()
     return 0
 
   of WM_MBUTTONDOWN, WM_MBUTTONDBLCLK:
-    io.mouseDown[2] = true
-    SetCapture(hwnd)
+    g_MouseJustPressed[2] = true
+    if GetCapture() == 0: SetCapture(hwnd)
     return 0
-
   of WM_MBUTTONUP:
     io.mouseDown[2] = false
-    ReleaseCapture()
+    if GetCapture() == hwnd: ReleaseCapture()
+    return 0
+
+  of WM_XBUTTONDOWN, WM_XBUTTONDBLCLK:
+    let btn = int(GET_XBUTTON_WPARAM(wParam))
+    if btn == XBUTTON1: g_MouseJustPressed[3] = true
+    else: g_MouseJustPressed[4] = true
+    if GetCapture() == 0: SetCapture(hwnd)
+    return 0
+  of WM_XBUTTONUP:
+    let btn = int(GET_XBUTTON_WPARAM(wParam))
+    if btn == XBUTTON1: io.mouseDown[3] = false
+    else: io.mouseDown[4] = false
+    if GetCapture() == hwnd: ReleaseCapture()
     return 0
 
   of WM_MOUSEWHEEL:
-    io.mouseWheel += float32(GET_WHEEL_DELTA_WPARAM(wParam)) / float32(WHEEL_DELTA)
+    io.mouseWheel += (float32(GET_WHEEL_DELTA_WPARAM(wParam)) / float32(WHEEL_DELTA))
     return 0
-
   of WM_MOUSEHWHEEL:
-    io.mouseWheelH += float32(GET_WHEEL_DELTA_WPARAM(wParam)) / float32(WHEEL_DELTA)
-    return 0
-
-  of WM_KEYDOWN, WM_SYSKEYDOWN:
-    if wParam < 256:
-      io.keysDown[wParam] = true
-    return 0
-
-  of WM_KEYUP, WM_SYSKEYUP:
-    if wParam < 256:
-      io.keysDown[wParam] = false
+    io.mouseWheelH += (float32(GET_WHEEL_DELTA_WPARAM(wParam)) / float32(WHEEL_DELTA))
     return 0
 
   of WM_CHAR:
-    if wParam > 0 and wParam < 0x10000:
-      io.addInputCharacterUTF16(uint16(wParam))
+    # Legacy input path (works with NimGL bindings)
+    let c = uint32(wParam)
+    if c > 0'u32 and c < 0x10000'u32:
+      io.addInputCharacter(cast[ImWchar](c))
     return 0
 
   of WM_SETCURSOR:
-    if LOWORD(lParam) == HTCLIENT and igWin32UpdateMouseCursor():
-      return 1
+    # Let NewFrame handle cursor; we still return 0 so default proc can run too.
     return 0
 
   else:
-    return 0
+    discard
+
+  return 0
+
+proc igWin32NewFrame*() =
+  let io = igGetIO()
+  assert g_hWnd != 0
+
+  # Display size
+  var rect: RECT
+  GetClientRect(g_hWnd, addr rect)
+  io.displaySize = ImVec2(
+    x: float32(rect.right - rect.left),
+    y: float32(rect.bottom - rect.top)
+  )
+
+  # Time step
+  var currentTime: int64
+  QueryPerformanceCounter(cast[ptr LARGE_INTEGER](addr currentTime))
+  io.deltaTime = float32(currentTime - g_Time) / float32(g_TicksPerSecond)
+  g_Time = currentTime
+
+  # Mouse buttons (combine "just pressed" from WndProc with current key state)
+  io.mouseDown[0] = g_MouseJustPressed[0] or ((GetKeyState(VK_LBUTTON) and 0x8000) != 0)
+  io.mouseDown[1] = g_MouseJustPressed[1] or ((GetKeyState(VK_RBUTTON) and 0x8000) != 0)
+  io.mouseDown[2] = g_MouseJustPressed[2] or ((GetKeyState(VK_MBUTTON) and 0x8000) != 0)
+  io.mouseDown[3] = g_MouseJustPressed[3]
+  io.mouseDown[4] = g_MouseJustPressed[4]
+  for i in 0 ..< g_MouseJustPressed.len:
+    g_MouseJustPressed[i] = false
+
+  # Mouse pos
+  var p: POINT
+  if GetCursorPos(addr p) != 0 and ScreenToClient(g_hWnd, addr p) != 0:
+    io.mousePos = ImVec2(x: float32(p.x), y: float32(p.y))
+  else:
+    io.mousePos = ImVec2(x: -1e10'f32, y: -1e10'f32)
+
+  # Keys (legacy keysDown[0..255])
+  for vk in 0 .. 255:
+    io.keysDown[vk] = (GetKeyState(vk.int32) and 0x8000) != 0
+
+  # Modifiers
+  io.keyCtrl  = (GetKeyState(VK_CONTROL) and 0x8000) != 0
+  io.keyShift = (GetKeyState(VK_SHIFT) and 0x8000) != 0
+  io.keyAlt   = (GetKeyState(VK_MENU) and 0x8000) != 0
+  io.keySuper = (GetKeyState(VK_LWIN) and 0x8000) != 0 or (GetKeyState(VK_RWIN) and 0x8000) != 0
+
+  # Cursor update (safe indexing)
+  if (io.configFlags.int32 and ImGuiConfigFlags.NoMouseCursorChange.int32) == 0:
+    var c = igGetMouseCursor().int32
+    if io.mouseDrawCursor:
+      SetCursor(0)
+    else:
+      if c < 0: c = 0
+      if c >= int32(g_MouseCursors.len): c = 0
+      if c != g_LastMouseCursor:
+        g_LastMouseCursor = c
+        let hcur = g_MouseCursors[c]
+        if hcur != 0: SetCursor(hcur) else: SetCursor(g_MouseCursors[0])
